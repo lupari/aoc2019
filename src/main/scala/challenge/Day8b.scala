2f3a40eb9d40f92486b8b6ce70e5d4eb28917e5c
Matti Lupari <matti.lupari@arcusys.fi>
2019-12-08 14:52:53 +0200

    Revert "Remove puzzle inputs from repository"
    This reverts commit 0763ee3bf7638785fa9fb649258d82873bda01c6.

--- src/main/scala/challenge/Day8b.scala
+++ src/main/scala/challenge/Day8b.scala
@@ -10,7 +10,7 @@ object Day8b extends Challenge {
     val input: List[Char]        = Source.fromResource("day8.txt").mkString.trim.toList
     val layers: List[List[Char]] = input.grouped(25 * 6).toList
     val picture: List[String] =
-      layers.transpose.map(p => p.dropWhile(_ == '2').head).grouped(25).map(_.mkString).toList
+      layers.transpose.map(_.dropWhile(_ == '2').head).grouped(25).map(_.mkString).toList
     println(picture.map(_ :+ '\n').mkString.replace('1', '#').replace('0', ' '))
     picture
   }
