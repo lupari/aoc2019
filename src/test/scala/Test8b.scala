81c9d9889224d883acffcdb6bab49ba7680ba143
Matti Lupari <matti.lupari@arcusys.fi>
2019-12-08 14:52:53 +0200

    Revert "Remove puzzle inputs from repository"
    This reverts commit 0763ee3bf7638785fa9fb649258d82873bda01c6.

--- src/test/scala/Test8b.scala
+++ src/test/scala/Test8b.scala
@@ -4,15 +4,7 @@ import org.scalatest._
 class Test8b extends FlatSpec with Matchers {
 
   it should "display AURCY" in {
-    Day8b.run() should be(
-      List(
-        "0110010010111000110010001",
-        "1001010010100101001010001",
-        "1001010010100101000001010",
-        "1111010010111001000000100",
-        "1001010010101001001000100",
-        "1001001100100100110000100"
-      )
-    )
+    val pic = Day8b.run().asInstanceOf[String]
+    println(pic)
   }
 }
