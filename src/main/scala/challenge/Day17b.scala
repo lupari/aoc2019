37ab6a3363fdcfdafa0f0102d7b650e801c34ede
Matti Lupari <matti.lupari@gmail.com>
2019-12-17 23:16:50 +0200

    Day 17 pt 2

--- src/main/scala/challenge/Day17b.scala
+++ src/main/scala/challenge/Day17b.scala
@@ -9,15 +9,15 @@ object Day17b extends Challenge {
 
   case class Point(x: Int, y: Int) {
     def neighbors = List(Point(x, y + 1), Point(x, y - 1), Point(x + 1, y), Point(x - 1, y))
-    def next(dir: Char): Point = dir match {
-      case 'U' => Point(x, y - 1)
-      case 'D' => Point(x, y + 1)
-      case 'L' => Point(x - 1, y)
-      case 'R' => Point(x + 1, y)
-    }
   }
 
   case class Dir(p: Point, dir: Char) {
+    def forward(): Dir = dir match {
+      case 'U' => copy(p = p.copy(y = p.y - 1))
+      case 'D' => copy(p = p.copy(y = p.y + 1))
+      case 'L' => copy(p = p.copy(x = p.x - 1))
+      case 'R' => copy(p = p.copy(x = p.x + 1))
+    }
     def rotate(clockwise: Boolean): Dir = dir match {
       case 'U' => if (clockwise) Dir(Point(p.x + 1, p.y), 'R') else Dir(Point(p.x - 1, p.y), 'L')
       case 'D' => if (clockwise) Dir(Point(p.x - 1, p.y), 'L') else Dir(Point(p.x + 1, p.y), 'R')
@@ -43,21 +43,19 @@ object Day17b extends Challenge {
     acc(input, Map.empty.withDefaultValue('.'), Point(0, 0))
   }
 
-  def isIntersection(p: Point, grid: Map[Point, Char]): Boolean = p.neighbors.forall(grid(_) == '#')
-
   def getPath(grid: Map[Point, Char]): List[Char] = {
-    def start = grid.find(_._2 == '^').get
 
+    def start = grid.find(e => "^><v".contains(e._2)).get
+    def symbolToDir(s: Char) = s match {
+      case '^' => 'U'
+      case 'v' => 'D'
+      case '<' => 'L'
+      case '>' => 'R'
+    }
     def nextNeighbor(dir: Dir): Option[Dir] = {
       val neighbors = dir.p.neighbors.filter(grid(_) == '#')
-      val nextInDir = dir.p.next(dir.dir)
-      if (neighbors.contains(nextInDir)) Some(dir.copy(p = nextInDir))
-      else {
-        val (left, right) = (dir.rotate(clockwise = false), dir.rotate(clockwise = true))
-        if (neighbors.contains(left.p)) Some(left)
-        else if (neighbors.contains(right.p)) Some(right)
-        else None
-      }
+      List(dir.forward(), dir.rotate(clockwise = false), dir.rotate(clockwise = true))
+        .find(d => neighbors.contains(d.p))
     }
 
     @tailrec
@@ -68,7 +66,7 @@ object Day17b extends Challenge {
       }
     }
 
-    acc(Dir(start._1, 'U'), Nil)
+    acc(Dir(start._1, symbolToDir(start._2)), Nil)
   }
 
   def compress(s: String): List[String] = {
@@ -78,30 +76,88 @@ object Day17b extends Challenge {
       case _ =>
         val c   = s.head
         val seg = s.takeWhile(_ == s.head)
-        val nextD = (dir, c) match {
-          case ('L', 'D') => 'L'
-          case ('R', 'D') => 'R'
-          case ('L', 'U') => 'R'
-          case ('L', 'D') => 'L'
-          case _          => c
+        val nextDir = (dir, c) match {
+          case ('L', 'D') | ('U', 'L') | ('D', 'R') | ('R', 'U') => 'L'
+          case ('D', 'L') | ('L', 'U') | ('R', 'D') | ('U', 'R') => 'R'
+          case _                                                 => c
         }
-        val nextS: String = nextD.toString + seg.length
-        _compress(s.drop(seg.length), c, acc :+ nextS)
+        _compress(s.drop(seg.length), c, acc :+ nextDir.toString + seg.length)
     }
-    _compress(s, ' ', Nil)
+    _compress(s, s.head, Nil)
+  }
+
+  def findMovements(path: List[String]): List[List[String]] = {
+
+    @tailrec
+    def acc(xs: List[String], ms: List[List[String]]): List[List[String]] = xs match {
+      case h :: i :: j :: k :: t =>
+        val threes: List[String] = List(h, i, j)
+        val fours: List[String]  = threes :+ k
+        acc(List(i, j, k) ++ t, ms :+ threes :+ fours)
+      case h :: i :: j :: t =>
+        val threes: List[String] = List(h, i, j)
+        acc(List(i, j) ++ t, ms :+ threes)
+      case _ => ms
+    }
+
+    @tailrec
+    def removeSubList(l: List[String], sl: List[String]): List[String] = l.indexOfSlice(sl) match {
+      case -1 => l
+      case i  => removeSubList(l.patch(i, Nil, sl.length), sl)
+    }
+
+    def isCompleteMatch(path: List[String], candidate: List[List[String]]) =
+      candidate.foldLeft(path)((a, b) => removeSubList(a, b)).isEmpty
+
+    acc(path, Nil).combinations(3).find(isCompleteMatch(path, _)).get
+  }
+
+  def mainRoutine(path: List[String], movements: List[List[String]]): List[Char] = {
+    def indexed(xs: List[String]): List[Int] =
+      path.zipWithIndex
+        .sliding(xs.length)
+        .map(l => (l.head._2, l.map(_._1)))
+        .filter(_._2 == xs)
+        .toList
+        .map(_._1)
+
+    val ia = indexed(movements.head).map((_, 'A'))
+    val ib = indexed(movements.drop(1).head).map((_, 'B'))
+    val ic = indexed(movements.last).map((_, 'C'))
+    (ia ++ ib ++ ic).sortBy(_._1).map(_._2)
+  }
+
+  def punctuated(xs: List[String]): List[Char] = {
+    def punctuate(s: String): List[Char] = s match {
+      case x if x.length == 2 => List(x.head, x.last, ',')
+      case _                  => List(s.head, ',')
+    }
+    xs.flatMap(x => List(x.head, x.drop(1))).map(_.toString).flatMap(punctuate).dropRight(1) :+ '\n'
+  }
+
+  def punctuated1(xs: List[Char]): List[Char] = xs.flatMap(List(_) :+ ',').dropRight(1) :+ '\n'
+
+  def dustCount(prog: ic.Program,
+                main: List[Char],
+                a: List[Char],
+                b: List[Char],
+                c: List[Char]): Long = {
+    val in: List[Char] = (main ++ a ++ b ++ c) ++ List('n', '\n')
+    ic.execute(ic.Input(prog.updated(0, 2), in.map(_.toInt))).sig.last
   }
 
   override def run(): Any = {
-    val program = ic.read(Source.fromResource("day17.txt"))
-    val output  = ic.execute(ic.Input(program, Nil)).sig.map(_.toInt)
+    val program   = ic.read(Source.fromResource("day17.txt"))
+    val output    = ic.execute(ic.Input(program, Nil)).sig.map(_.toInt)
+    val grid      = getGrid(output)
+    val path      = compress(getPath(grid).mkString)
+    val movements = findMovements(path)
+    val seqM      = punctuated1(mainRoutine(path, movements))
+    val seqA      = punctuated(movements.head)
+    val seqB      = punctuated(movements.drop(1).head)
+    val seqC      = punctuated(movements.last)
 
-    val grid = getGrid(output)
-
-    val path       = getPath(grid)
-    val compressed = compress(path.mkString)
-    println(compressed.mkString)
-
-    path
+    dustCount(program, seqM, seqA, seqB, seqC)
   }
 
 }
