6ed5d67c85c08446c94020892739dc426748aafe
Matti Lupari <matti.lupari@gmail.com>
2019-12-30 23:52:12 +0200

    Day 18 pt 2

--- src/main/scala/challenge/Day18b.scala
+++ src/main/scala/challenge/Day18b.scala
@@ -11,11 +11,11 @@ object Day18b extends Challenge {
 
   case class Point(x: Int, y: Int) {
     def neighbors = List(Point(x, y - 1), Point(x + 1, y), Point(x, y + 1), Point(x - 1, y))
-    def diags =
+    def corners =
       List(Point(x - 1, y - 1), Point(x + 1, y - 1), Point(x - 1, y + 1), Point(x + 1, y + 1))
   }
 
-  def getGrid(input: List[Char]): (Map[Point, Char], Point) = {
+  def getGrid(input: List[Char]): (Map[Point, Char], Seq[Point]) = {
 
     @tailrec
     def acc(xs: List[Char], grid: Map[Point, Char], current: Point): Map[Point, Char] = xs match {
@@ -29,18 +29,16 @@ object Day18b extends Challenge {
       case _ => grid
     }
 
-    val grid                        = acc(input, Map.empty.withDefaultValue(' '), Point(0, 0))
-    val start                       = grid.find(_._2 == '@').get
-    val newStarts: Map[Point, Char] = start._1.diags.map(d => (d -> '@')).toMap
-    val newWalls: Map[Point, Char]  = start._1.neighbors.map(n => (n -> '#')).toMap
-    ((grid ++ newStarts ++ newWalls).updated(start._1, '#'), start._1)
+    val grid      = acc(input, Map.empty.withDefaultValue(' '), Point(0, 0))
+    val start     = grid.find(_._2 == '@').get
+    val newStarts = start._1.corners.map(c => c -> '@').toMap
+    val newWalls  = (start._1 +: start._1.neighbors).map(n => n -> '#').toMap
+    (grid ++ newStarts ++ newWalls, newStarts.keys.toSeq)
   }
 
-  def search(grid: Map[Point, Char], start: Point, keys: Set[Char]) = {
+  case class KeyFoundEvent(dist: Int, point: Point, key: Char)
+  def search(grid: Map[Point, Char], start: Point, keys: Set[Char]): List[KeyFoundEvent] = {
     case class State(p: Point)
-    case class KeyFoundEvent(dist: Int, point: Point, key: Char)
-
-    // THIS SHOULD BE AN ITERATOR OR SMTH LIKE THAT
 
     @tailrec
     def bfs(queue: Queue[(State, Int)],
@@ -57,9 +55,8 @@ object Day18b extends Challenge {
               bfs(q, seen + state, events) // door, no key
             case c =>
               c match {
-                case k if k.isLetter && k.isLower => // found a key
-                  val states = state.p.neighbors.map(n => (State(n), dist + 1))
-                  bfs(q ++ states, seen + state, events :+ KeyFoundEvent(dist, state.p, k))
+                case k if k.isLetter && k.isLower && !keys.contains(k) => // found a new key
+                  bfs(q, seen, events :+ KeyFoundEvent(dist, state.p, k))
                 case _ =>
                   val states = state.p.neighbors.map(n => (State(n), dist + 1))
                   bfs(q ++ states, seen + state, events)
@@ -72,57 +69,43 @@ object Day18b extends Challenge {
 
   }
 
-  override def run(): Any = {
-    val input         = Source.fromResource("day18.txt").mkString.toList
-    val (grid, start) = getGrid(input)
-    val (x, y)        = (grid.keys.maxBy(_.x).x, grid.keys.maxBy(_.y).y)
-    val canvas        = Array.tabulate(y + 1, x + 1)((_, _) => ' ')
-    for { p <- grid } yield canvas(p._1.y)(p._1.x) = p._2
-    canvas foreach { row =>
-      row.mkString foreach print; println
-    }
-    val starts  = start.diags
-    val allKeys = grid.filter(_._2.isLower).map(_._2).toSet
+  def searchVaults(grid: Map[Point, Char], entrances: Seq[Point], keys: Set[Char]): Int = {
+    case class GlobalState(dist: Int, vaults: Seq[Point], keys: Set[Char])
 
-    case class GlobalState(dist: Int, vaults: List[Point], keys: Set[Char])
     val seen: mutable.Map[Int, Set[(Point, Set[Char])]] =
       mutable.Map(0 -> Set(), 1 -> Set(), 2 -> Set(), 3 -> Set())
 
     val pq = mutable.PriorityQueue[GlobalState]()(Ordering.by((gs: GlobalState) => -gs.dist))
-    pq.enqueue(GlobalState(0, starts, Set.empty))
-    while (pq.nonEmpty && pq.head.keys != allKeys) {
+    pq.enqueue(GlobalState(0, entrances, Set.empty))
+
+    def step(): Option[Int] = {
       val gs = pq.dequeue()
-      if (gs.keys == allKeys) {
-        println(gs.dist)
-      } else {
-        gs.vaults.zipWithIndex.foreach({
-          case (point, i) if !seen(i).contains((point, gs.keys)) =>
-            seen + (i -> (seen(i) + ((point, gs.keys))))
-            val events = search(grid, point, gs.keys)
-            events.foreach(e => {
-              //println("key found: " + (i, e))
-              pq.enqueue(
-                GlobalState(gs.dist + e.dist, gs.vaults.updated(i, e.point), gs.keys + e.key))
-            })
-          //println("done with " + i)
-        })
+      gs.keys match {
+        case gk if gk.size == keys.size => Some(gs.dist)
+        case _ =>
+          gs.vaults.zipWithIndex.foreach({
+            case (point, i) =>
+              if (!seen(i).contains((point, gs.keys))) {
+                seen(i) = seen(i) + ((point, gs.keys))
+                val events = search(grid, point, gs.keys)
+                events.foreach(e => {
+                  pq.enqueue(
+                    GlobalState(gs.dist + e.dist, gs.vaults.updated(i, e.point), gs.keys + e.key))
+                })
+              }
+          })
+          None
       }
     }
 
-    println("ready now?")
+    Iterator.continually(step()).dropWhile(_.isEmpty).next.get
+  }
 
-    // Setup a registry of seen places for each vault (Set(), Set(), Set(), Set())
-    // Setup a priority queue of (dist -> (points, keys))
-    // initially: (0 -> starts, empty set)
-    // while queue not empty
-    // dequeue : d -> (points, keys)
-    // if (keys == allkeys) yield d
-    // for all points.zipWithIndex i, p
-    // if (p, keys) in seen(i) continue
-    // else seen(i) :+ (p, keys)
-    // for d2, p2, key in search(p, keys)
-    // q.push(d + d2 -> points(i) = p2, keys :+ key)
-
+  override def run(): Any = {
+    val input          = Source.fromResource("day18.txt").mkString.toList
+    val (grid, starts) = getGrid(input)
+    val allKeys        = grid.filter(_._2.isLower).values.toSet
+    searchVaults(grid, starts, allKeys)
   }
 
 }
--- src/test/scala/Test18b.scala
+++ src/test/scala/Test18b.scala
@@ -4,6 +4,6 @@ import org.scalatest._
 class Test18b extends flatspec.AnyFlatSpec with matchers.should.Matchers {
 
   it should "calculate correct result" in {
-    Day18b.run() should be(3048)
+    Day18b.run() should be(1732)
   }
 }
